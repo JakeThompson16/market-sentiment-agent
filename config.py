4eea37cb7fc906824c5b6e548359f2b3a30d8d68
JakeThompson16 <jake.thompson001@umb.edu>
2026-06-14 20:49:24 -0400

    config variables

---
+++ config.py
@@ -0,0 +1,28 @@
+
+RELEVANT_TICKERS = [
+    "NVDA",
+    "AAPL",
+    "GOOGL",
+    "MSFT",
+    "AMZN",
+    "TSM",
+    "TSLA",
+    "META",
+    "BRK.B",
+    "LLY",
+    "AVGO",
+    "MRVL",
+    "PLTR",
+    "AMD",
+    "SNDK",
+    "MU",
+    "MRNA",
+    "TPL"
+]
+"""General tickers to use for market sentiment"""
+
+SENTIMENT_SYSTEM_PROMPT_PATH = "system_docs/sentiment_system_prompt.md"
+"""Path to sentiment analyst prompt markdown file"""
+
+SUPERVISOR_SYSTEM_PROMPT_PATH = "system_docs/supervisor_system_prompt.md"
+"""Path to supervisor prompt markdown file"""
